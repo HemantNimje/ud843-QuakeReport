ba078f186563471680bece9a2074ed5c51b8dc7a
Hemant Nimje <nimje.hemant@gmail.com>
2017-11-19 23:58:37 -0800

    Add QueryUtils.java to Parse JSONResponse in QuakeReport app.

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -3,55 +3,55 @@ package com.example.android.quakereport;
 public class Earthquake {
 
     // Magnitude of the earthquake
-    private String mMagnitude;
+    private double mMagnitude;
 
     // Location of the earthquake
     private String mLocation;
 
     // Date of the earthquake
-    private String mDate;
+    private long mDate;
 
     /**
      * Create new object of Earthquake
      *
      * @param magnitude is magnitude of the earthquake
-     * @param location is location of the earthquake
-     * @param date is date of the earthquake
+     * @param location  is location of the earthquake
+     * @param date      is date of the earthquake
      */
 
-    public Earthquake(String magnitude, String location, String date){
+    public Earthquake(double magnitude, String location, long date) {
         mMagnitude = magnitude;
         mLocation = location;
         mDate = date;
     }
 
     // Get the magnitude of the earthquake
-    public String getMagnitude(){
+    public double getMagnitude() {
         return mMagnitude;
     }
 
     // Get the location of the earthquake
-    public String getLocation(){
+    public String getLocation() {
         return mLocation;
     }
 
     // Get the date of the earthquake
-    public String getDate(){
+    public long getDate() {
         return mDate;
     }
 
     // Set the magnitude of the earthquake
-    public void setMagnitude(String magnitude){
+    public void setMagnitude(double magnitude) {
         mMagnitude = magnitude;
     }
 
     // Set the location of the earthquake
-    public void setLocation(String location){
+    public void setLocation(String location) {
         mLocation = location;
     }
 
     // Set the date of the earthquake
-    public void setDate(String date){
+    public void setDate(long date) {
         mDate = date;
     }
 
--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -51,7 +52,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView earthquakeMagnitude = (TextView) listItemView.findViewById(R.id.earthquake_magnitude);
 
         // Set the magnitude TextView with current earthquake magnitude
-        earthquakeMagnitude.setText(currentEarthquake.getMagnitude());
+        earthquakeMagnitude.setText(String.valueOf(currentEarthquake.getMagnitude()));
 
         // Find the TextView for the location from layout
         TextView earthquakeLocation = (TextView) listItemView.findViewById(R.id.earthquake_location);
@@ -63,7 +64,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView earthquakeDate = (TextView) listItemView.findViewById(R.id.earthquake_date);
 
         // Set the date TextView with current earthquake date
-        earthquakeDate.setText(currentEarthquake.getDate());
+        earthquakeDate.setText(String.valueOf(currentEarthquake.getDate()));
 
         // Return listItem layout
         return listItemView;
